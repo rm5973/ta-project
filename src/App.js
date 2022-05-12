9f0e692d6d3d5fcc256535ca826bded352c7d204
rm5973 <masurnaikrakshit@gmail.com>
2022-05-12 23:40:20 +0530

    complete updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-
-
+import Search from "./compo/search";
+import Card from "./compo/Card";
 function App() {
   return (
-    <div> <h1 class="text-blue-500 text-[30vw]"> hello</h1></div>
+    <div class='bg-inherit bg-slate-600 h-[70vw]  ' >  
+        <div><Search />
+          </div>
+          <div class="flex justify-center items-center">
+            <Card />
+          </div>
+        
+     
+    </div>
   );
 }
 
---
+++ src/compo/Card.jsx
@@ -0,0 +1,117 @@
+import images from "../images/images.png"
+import React from "react"
+export default function Card() {
+    return (
+        <div  >
+            <div class="border-0 bg-[#B7D3F2] shadow-md w-[10vw] h-[15vw]  mx-auto  rounded-sm">
+                <div>
+
+                    <img src={images} alt="img not found" class=" rounded-t-[1.5vw] h-[10vw] w-[10vw] p-2 " />
+                </div>
+                <div class=' text-center '>
+                    <p class=' text-[0.5vw] text-white'>
+                        An X'perience b'yond mere learning!
+                    </p>
+
+                    <h1 class='text-[#79BEEE] py-1 text-[1vw]'>
+                        SINCE 2015
+                    </h1>
+                    <p className="text-white underline text-[0.5vw]">
+                        lora lumsum
+                    </p>
+                </div>
+
+            </div>
+            <div class=' py-2 '>
+                <div class=' bg-cyan-500 border-cyan-500 h-[8vw] border-[0.1vw] w-[0vw] mx-auto'> </div>
+
+
+            </div>
+
+    <div class='grid grid-cols-3 grid-flow-col gap-4 py-2'>
+        <div class="border-0 bg-[#B7D3F2] shadow-md w-[10vw] h-[15vw]    rounded-sm">
+                <div>
+
+                    <img src={images} alt="img not found" class=" rounded-t-[1.5vw] h-[10vw] w-[10vw] p-2 " />
+                </div>
+                <div class=' text-center '>
+                    <p class=' text-[0.5vw] text-white'>
+                        An X'perience b'yond mere learning!
+                    </p>
+
+                    <h1 class='text-[#79BEEE] py-1 text-[1vw]'>
+                        SINCE 2015
+                    </h1>
+                    <p className="text-white underline text-[0.5vw]">
+                        lora lumsum
+                    </p>
+                </div>
+
+            </div>
+
+            <div class="border-0 bg-[#B7D3F2] shadow-md w-[10vw] h-[15vw]    rounded-sm">
+                <div>
+
+                    <img src={images} alt="img not found" class=" rounded-t-[1.5vw] h-[10vw] w-[10vw] p-2 " />
+                </div>
+                <div class=' text-center '>
+                    <p class=' text-[0.5vw] text-white'>
+                        An X'perience b'yond mere learning!
+                    </p>
+
+                    <h1 class='text-[#79BEEE] py-1 text-[1vw]'>
+                        SINCE 2015
+                    </h1>
+                    <p className="text-white underline text-[0.5vw]">
+                        lora lumsum
+                    </p>
+                </div>
+
+            </div>
+            <div class="border-0 bg-[#B7D3F2] shadow-md w-[10vw] h-[15vw]    rounded-sm">
+                <div>
+
+                    <img src={images} alt="img not found" class=" rounded-t-[1.5vw] h-[10vw] w-[10vw] p-2 " />
+                </div>
+                <div class=' text-center '>
+                    <p class=' text-[0.5vw] text-white'>
+                        An X'perience b'yond mere learning!
+                    </p>
+
+                    <h1 class='text-[#79BEEE] py-1 text-[1vw]'>
+                        SINCE 2015
+                    </h1>
+                    <p className="text-white underline text-[0.5vw]">
+                        lora lumsum
+                    </p>
+                </div>
+
+            </div>
+            <div class="border-0 bg-[#B7D3F2] shadow-md w-[10vw] h-[15vw]    rounded-sm">
+                <div>
+
+                    <img src={images} alt="img not found" class=" rounded-t-[1.5vw] h-[10vw] w-[10vw] p-2 " />
+                </div>
+                <div class=' text-center '>
+                    <p class=' text-[0.5vw] text-white'>
+                        An X'perience b'yond mere learning!
+                    </p>
+
+                    <h1 class='text-[#79BEEE] py-1 text-[1vw]'>
+                        SINCE 2015
+                    </h1>
+                    <p className="text-white underline text-[0.5vw]">
+                        lora lumsum
+                    </p>
+                </div>
+
+            </div>
+    </div>
+
+
+
+</div>
+
+        
+    )
+}
